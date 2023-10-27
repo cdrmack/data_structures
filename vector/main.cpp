361ad4f673819e3ce51ade7ef88454f5de5f567f
cdrmack <git.pynvt@passmail.net>
2023-10-27 12:01:44 +0200

    vector: add `push_back` (without growing yet)

--- vector/main.cpp
+++ vector/main.cpp
@@ -7,6 +7,9 @@ constexpr std::size_t initial_vector_size = 10;
 auto main(int argc, char *argv[]) -> int
 {
     vector<int32_t, initial_vector_size> v = {};
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(42);
 
     return 0;
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -5,6 +5,22 @@
 template <typename T, std::size_t S>
 class vector
 {
+public:
+    vector() : m_size(0), m_capacity(S) {};
+
+    auto push_back(const T& element) -> void
+    {
+        if (m_size == m_capacity)
+        {
+            // TODO, grow
+        }
+
+        m_data[m_size] = element;
+        ++m_size;
+    }
+
 private:
-    T data[S];
+    std::size_t m_size;
+    std::size_t m_capacity;
+    T m_data[S];
 };

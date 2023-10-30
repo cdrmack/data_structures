c9b86642295bb0c4c514d1ebb49d46048c6af0f6
cdrmack <git.pynvt@passmail.net>
2023-10-30 14:16:16 +0100

    vector: add `grow` functionality

--- vector/main.cpp
+++ vector/main.cpp
@@ -2,13 +2,16 @@
 
 #include "vector.hpp"
 
-constexpr std::size_t initial_vector_size = 10;
+constexpr std::size_t initial_vector_size = 5;
 
 auto main(int argc, char *argv[]) -> int
 {
     vector<int32_t, initial_vector_size> v = {};
     v.push_back(1);
     v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
     v.push_back(42);
 
     return 0;
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,18 +1,25 @@
 #pragma once
 
 #include <cstddef>
+#include <cstring>
 
 template <typename T, std::size_t S>
 class vector
 {
 public:
-    vector() : m_size(0), m_capacity(S) {};
+    vector() : m_size(0), m_capacity(S), m_data(new T[S])
+    {};
+
+    ~vector()
+    {
+        delete[] m_data;
+    };
 
     auto push_back(const T& element) -> void
     {
         if (m_size == m_capacity)
         {
-            // TODO, grow
+            grow();
         }
 
         m_data[m_size] = element;
@@ -20,7 +27,18 @@ public:
     }
 
 private:
+    auto grow() -> void
+    {
+        std::size_t new_capacity = m_capacity * 2;
+        T* new_data = new T[new_capacity];
+
+        std::memcpy(new_data, m_data, m_capacity * sizeof(T));
+        delete[] m_data;
+        m_data = new_data;
+        m_capacity = new_capacity;
+    };
+
     std::size_t m_size;
     std::size_t m_capacity;
-    T m_data[S];
+    T* m_data;
 };
